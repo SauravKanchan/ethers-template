90c56534b705f479f0fa2a30a001398a3c8fbc01
SauravKanchan <sauravnk30@gmail.com>
2019-09-23 09:13:49 +0530

    remove redundant send function from app.js

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const ethers = require('ethers');
 const config = require('./config.json');
 
 // Import the json file from build to get the abi
-const erc_json = require('./build/ERC20.json');
+const erc_json = require('./build/ERC20.json'); //import the json of the contract which you want to interact
 
 // You can use any standard network name
 //  - "homestead"
@@ -18,21 +18,8 @@ const wallet = new ethers.Wallet(config['private_key'] , provider);
 const address = config["ERC20"];
 const abi = erc_json.abi;
 
-const erc20 = new ethers.Contract( address , abi , wallet );
+erc20 = new ethers.Contract( address , abi , wallet );
 
-async function send() {
-	let address = document.getElementById("address").value;
-	let tx = await erc20.functions.transfer("0xCA35b7d915458EF540aDe6068dFe2F44E8fa733c", "1000000000000000");
-	let tx_hash = tx.hash
-	let node = document.createElement("LI");
-	let link = document.createElement("A");
-	link.target = "_blank";
-	link.href = "https://kovan.etherscan.io/tx/" + tx_hash;
-	let textnode = document.createTextNode(tx_hash);
-	link.appendChild(textnode);
-	node.appendChild(link);
-	document.getElementById("transactions").appendChild(node);
-}
 document.getElementById("address").onclick = async function() {
 	let address = document.getElementById("address").value;
 	let tx = await erc20.functions.transfer(address, "1000000000000000000");
